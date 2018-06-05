903b35b58dca1596c2120a86046d3622c554942d
mVang <meng.vang92@yahoo.com>
2018-06-04 20:07:35 -0500

    More stuff

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // Append character images to the screen
+    $("#character1").append('<img src="assets/images/F16.jpg" alt="F16 Fighter Jet" style="width:100%"> <b>F16 Fighter Jet</b>')
+    $("#character2").append('<img src="assets/images/F18.jpg" alt="F18 Fighter Jet" style="width:100%"> <b>F18 Fighter Jet</b>')
+    $("#character3").append('<img src="assets/images/Mig 25.jpg" alt="Mig 25 Fighter Jet" style="width:100%"> <b>Mig 25 Fighter Jet</b>')
+    $("#character4").append('<img src="assets/images/Mig 29.jpg" alt="Mig 25 Fighter Jet" style="width:100%"> <b>Mig 29 Fighter Jet</b>')
+
+});
